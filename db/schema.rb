7677d8eff9e5202df45481cdf9f4871ea75dd179
Jonathan East <jonathan_east@ymail.com>
2020-09-22 17:10:51 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 1) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
     t.integer  "patient_id"
     t.integer  "doctor_id"
-  end
-
-  create_table "doctors", force: :cascade do |t|
-    t.string "name"
-    t.string "department"
   end
 
 end
